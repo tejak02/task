5b0ee1b6d09dd1898039a5128a2a27f279fd4bb5
tejak02 <tejak190702@gmail.com>
2026-06-03 18:56:11 +0530

    Add files via upload

---
+++ javascript/first.js
@@ -0,0 +1,79 @@
+let name = "Teja";
+console.log(name);
+
+
+
+let age = 23;
+age = 24;
+
+console.log(age);
+
+
+const country = "India";
+
+console.log(country);
+
+
+
+
+let city = "Hyderabad";
+const state = "Telangana";
+
+console.log(city);
+console.log(state);
+
+
+
+
+
+let name = "Teja";
+
+console.log(typeof name);
+
+let age = 23;
+
+console.log(typeof age);
+
+let isStudent = true;
+
+console.log(typeof isStudent);
+
+
+
+
+
+
+
+let age = 20;
+
+if (age >= 18) {
+  console.log("Eligible to vote");
+}
+
+
+
+let age = 16;
+
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
+
+
+let x = 20;
+let y = 4;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+
+
+let num = 10;
+
+num += 5; 
+console.log(num); 
+
+
